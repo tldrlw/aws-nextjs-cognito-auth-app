ed2282b129a334bd8a4cce7faf63ba7afe0cd6b3
Refayat Haque <refayathaque@refayats-mbp.mynetworksettings.com>
2024-09-20 15:02:12 -0400

    works now

--- infrastructure/route-53.tf
+++ infrastructure/route-53.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "root_domain" {
   zone_id = data.aws_route53_zone.tldrlw_com.zone_id
   name    = var.HOSTNAME_SUBDOMAIN
-  # will show up in management console Route 53 as 'blog.tldrlw.com'
+  # will show up in management console Route 53 as 'cognito-test.tldrlw.com'
   type = "A"
   alias {
     name                   = module.main.alb_dns_name
